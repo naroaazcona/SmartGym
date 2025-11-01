c6a86abc005ca065aad7b3ddacf067e08711e796
naroaazcona <naroa.azcona@opendeusto.es>
2025-11-01 10:42:17 +0100

    puertos añadidos

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -1,8 +1,9 @@
 const express = require('express'); 
 const { createProxyMiddleware } = require('http-proxy-middleware'); 
 const cors = require('cors'); 
- 
 const app = express(); 
+const PORT = process.env.PORT || 3000;
+ 
  
 app.use(cors()); 
 app.use(express.json()); 
